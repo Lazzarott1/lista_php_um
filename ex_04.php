e219470ec5829f362f56e5cb72d47e68de3de835
Lazzarott1 <lucas_lazzarotti@estudante.sesisenai.org.br>
2026-07-09 10:53:29 -0300

    add - finalizado ex04

--- ex_04.php
+++ ex_04.php
@@ -7,14 +7,40 @@ números e caracteres especiais. -->
 <?php
 
 function gerarSenha($n) {
+    $maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $minusculas = 'abcdefghijklmnopqrstuvwxyz';
+    $numeros = '0123456789';
+    $especiais = '!@#$%^&*()-_=+';
 
-$senha = random_bytes($n);
-echo $senha;
+    $senha = '';
 
+    $nmaiusculas = strlen($maiusculas);
+    $nminusculas = strlen($minusculas);
+    $nnumeros = strlen($numeros);
+    $nespeciais = strlen($especiais);
+
+    for ($i = 0; $i < $n; $i++) {
+ 
+        $senha .= $maiusculas[random_int(0, $nmaiusculas)-1];
+        $i++;
+
+        if ($i >= $n) break;
+
+        $senha .= $minusculas[random_int(0, $nminusculas)-1];
+        $i++;
+
+        if ($i >= $n) break;
+        
+        $senha .= $numeros[random_int(0, $nnumeros)-1];
+        $i++;
+
+        if ($i >= $n) break;
+        
+        $senha .= $especiais[random_int(0, $nespeciais)-1];
+    }
+
+    return str_shuffle($senha);
 }
 
-$n = 9;
-
-gerarSenha($n);
-
-?>
+$n = 10;
+echo gerarSenha($n);
